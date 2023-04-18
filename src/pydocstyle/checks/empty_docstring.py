03da4852e72b32a387c2f6c0dc442fc1bea1f75e
Felipe Peter <mr-peipei@web.de>
2023-04-18 12:08:39 +0900

    Convert empty docstring check

---
+++ src/pydocstyle/checks/empty_docstring.py
@@ -0,0 +1,28 @@
+"""Contains a check for empty docstrings."""
+
+
+from typing import Optional
+
+from astroid import NodeNG
+
+from pydocstyle import (
+    CHECKED_NODE_TYPES,
+    NODES_TO_CHECK,
+    Configuration,
+    Docstring,
+    DocstringError,
+    has_content,
+)
+
+
+class D419(DocstringError):
+    description = "Docstring is empty."
+    applicable_nodes = NODES_TO_CHECK
+    applicable_if_doc_string_is_empty = True
+
+    @classmethod
+    def check_implementation(
+        cls, node: CHECKED_NODE_TYPES, docstring: Optional[Docstring], config: Configuration
+    ) -> None:
+        if not has_content(docstring.content):
+            raise cls(node)
