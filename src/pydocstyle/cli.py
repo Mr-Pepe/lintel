ada0130806ccf283d4de5f5b6429e5ec55432bb3
Felipe Peter <mr-peipei@web.de>
2023-04-21 02:01:55 +0900

    Refactor configuration loading

--- src/pydocstyle/cli.py
+++ src/pydocstyle/cli.py
@@ -2,15 +2,20 @@
 import logging
 import sys
 from pathlib import Path
+from typing import Optional
 
 from astroid.exceptions import AstroidSyntaxError
+from rich.logging import RichHandler
+from typer import Exit, Option, Typer
 
-from ._check_source import check_source
-from ._config import Configuration, ConfigurationParser, IllegalConfiguration
+from pydocstyle import Configuration, Convention, IllegalConfiguration, check_source, load_config
 
 __all__ = ('main',)
 
 _logger = logging.getLogger(__name__)
+
+
+app = Typer(rich_help_panel=True)
 
 
 class ReturnCode:
@@ -19,52 +24,99 @@ class ReturnCode:
     invalid_options = 2
 
 
-def run_pydocstyle():
-    _logger.setLevel(logging.DEBUG)
-    conf = ConfigurationParser()
-    setup_stream_handlers(conf.get_default_run_configuration())
+@app.command()
+def run(
+    config_path: Optional[Path] = Option(
+        None,
+        "--config",
+        help="A configuration file to use instead of using config discovery.",
+        show_default=False,
+    ),
+    convention: str = Option(
+        Convention.PEP257.value,
+        help=f"The convention to use. Must be one of {[c.value for c in Convention]}. "
+        "Add/remove error codes via the select/ignore options. "
+        "The final set of error codes is determined by taking the error codes defined by the "
+        "convention, then adding the error codes specified by the --select option and then "
+        "removing error codes specified by the --ignore option.",
+    ),
+    select: str = Option(
+        "",
+        help="A comma-separated list of error codes to check for. "
+        "The specified error codes will be added to the selected convention. "
+        f"Use the {Convention.NONE.value!r} convention to only check for the error codes "
+        "specified here.",
+    ),
+    ignore: str = Option(
+        "",
+        help="A comma-separated list of error codes to ignore. "
+        "The specified error codes will be removed from the selected convention. "
+        f"Use the {Convention.ALL.value!r} convention to select all error codes "
+        "and exclude specific error codes here.",
+    ),
+    match: Optional[str] = Option(
+        None,
+        help="Check only files that exactly match this regular expression. "
+        "The default matches Python files that don't start with 'test_'.",
+        show_default=False,
+    ),
+    match_dir: Optional[str] = Option(
+        None,
+        help="Search only directories that exactly match this regular expression. "
+        "The default matches all directories that don't start with a dot.",
+        show_default=False,
+    ),
+    ignore_decorators: Optional[str] = Option(
+        None,
+        help="Ignore any functions or methods that are decorated "
+        "by a function with a name matching this regular expression. "
+        "The default  does not ignore any decorated functions.",
+        show_default=False,
+    ),
+    property_decorators: Optional[str] = Option(
+        None,
+        help="A comma-separated list of property decorators. "
+        "Consider any method decorated with one of these "
+        "decorators as a property and consequently allow "
+        "a docstring which is not in imperative mood.",
+        show_default=False,
+    ),
+    ignore_inline_noqa: Optional[bool] = Option(
+        None, help="Whether inline `# noqa` comments are respected or not."
+    ),
+    verbose: Optional[bool] = Option(None, help="Whether to show more detailed output."),
+):
+    """Check docstring style."""
+    configure_logging(verbose)
 
-    try:
-        conf.parse()
-    except IllegalConfiguration:
-        return ReturnCode.invalid_options
+    config = load_config(config_path=config)
+    config = ConfigurationParser()
+    config.parse()
 
-    run_conf = conf.get_user_run_configuration()
+    run_conf = config.get_user_run_configuration()
 
     # Reset the logger according to the command line arguments
-    setup_stream_handlers(run_conf)
-
-    _logger.debug("starting in debug mode.")
+    configure_logging(run_conf)
 
     error_count = 0
-    try:
-        for (
-            filename,
-            checked_codes,
-            ignore_decorators,
-            property_decorators,
-        ) in conf.get_files_to_check():
-            _logger.info("Checking file: %s" % filename)
 
-            config = Configuration(
-                select=checked_codes,
-                ignore_decorators=ignore_decorators,
-                property_decorators=property_decorators,
-            )
+    for filename in []:
+        _logger.info("Checking file: %s" % filename)
 
-            try:
-                for error in check_source(Path(filename), config):
-                    sys.stdout.write('%s\n' % error)
-                    error_count += 1
+        config = Configuration(
+            select=checked_codes,
+            ignore_decorators=ignore_decorators,
+            property_decorators=property_decorators,
+        )
 
-            except AstroidSyntaxError:
-                sys.stderr.write(f"{filename}: Cannot parse file")
+        try:
+            for error in check_source(Path(filename), config):
+                sys.stdout.write('%s\n' % error)
                 error_count += 1
 
-    except IllegalConfiguration as error:
-        # An illegal configuration file was found during file generation.
-        _logger.error(error.args[0])
-        return ReturnCode.invalid_options
+        except AstroidSyntaxError:
+            sys.stderr.write(f"{filename}: Cannot parse file")
+            error_count += 1
 
     if error_count == 0:
         exit_code = ReturnCode.no_violations_found
@@ -75,36 +127,13 @@ def run_pydocstyle():
     return exit_code
 
 
-def main():
-    """Run pydocstyle as a script."""
-    try:
-        sys.exit(run_pydocstyle())
-    except KeyboardInterrupt:
-        pass
+def configure_logging(verbose: bool):
+    """Set up logging."""
 
+    rich_handler = RichHandler(rich_tracebacks=True)
+    rich_handler.setFormatter(logging.Formatter("%(message)s"))
 
-def setup_stream_handlers(conf):
-    """Set up logging stream handlers according to the options."""
-
-    class StdoutFilter(logging.Filter):
-        def filter(self, record):
-            return record.levelno in (logging.DEBUG, logging.INFO)
-
-    _logger.handlers = []
-
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setLevel(logging.WARNING)
-    stdout_handler.addFilter(StdoutFilter())
-    if conf.debug:
-        stdout_handler.setLevel(logging.DEBUG)
-    elif conf.verbose:
-        stdout_handler.setLevel(logging.INFO)
-    else:
-        stdout_handler.setLevel(logging.WARNING)
-    _logger.addHandler(stdout_handler)
-
-    stderr_handler = logging.StreamHandler(sys.stderr)
-    msg_format = "%(levelname)s: %(message)s"
-    stderr_handler.setFormatter(logging.Formatter(fmt=msg_format))
-    stderr_handler.setLevel(logging.WARNING)
-    _logger.addHandler(stderr_handler)
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.WARNING,
+        handlers=[rich_handler],
+    )
---
+++ tests/unit/test_config.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+import pytest
+
+from pydocstyle import Configuration, Convention, IllegalConfiguration, load_config
+
+
+def test_default_config() -> None:
+    config = Configuration()
+
+    assert config.convention == Convention.PEP257
+    assert config.select == set()
+    assert config.ignore == set()
+    assert config.match != ""
+    assert config.match_dir != ""
+    assert config.ignore_decorators == None
+    assert config.property_decorators == {
+        "property",
+        "cached_property",
+        "functools.cached_property",
+    }
+    assert config.ignore_inline_noqa is False
+    assert config.verbose is False
+
+
+def test_load_config_returns_default_config_if_no_config_found(tmp_path: Path) -> None:
+    assert load_config(tmp_path) == Configuration()
+
+
+@pytest.mark.parametrize("config_file", ["ini_example.ini", "toml_example.toml"])
+def test_loads_ini_and_toml_files_if_specified(config_file: str, resource_dir: Path) -> None:
+    config = load_config(resource_dir / "configs" / config_file)
+
+    assert config.convention == Convention.GOOGLE
+    assert config.select == {"D100", "D200"}
+    assert config.ignore == {"D100", "D300"}
+    assert config.match == "abc"
+    assert config.match_dir == "def"
+    assert config.ignore_decorators == "[a-z].*"
+    assert config.property_decorators == {"abc", "def"}
+    assert config.ignore_inline_noqa is True
+    assert config.verbose is True
+
+
+@pytest.mark.parametrize("config_file", ["missing_section.ini", "missing_section.toml"])
+def test_raises_error_if_configuration_is_missing_correct_section(
+    config_file: str, resource_dir: Path
+) -> None:
+    with pytest.raises(IllegalConfiguration):
+        load_config(resource_dir / "configs" / config_file)
+
+
+@pytest.mark.parametrize("config_file", ["unknown_options.ini", "unknown_options.toml"])
+def test_raises_error_if_unknown_options_are_present(config_file: str, resource_dir: Path) -> None:
+    with pytest.raises(IllegalConfiguration):
+        load_config(resource_dir / "configs" / config_file)
+
+
+def test_raises_error_if_unparsable_file(tmp_path: Path) -> None:
+    (tmp_path / "weird.bla").write_text("/([)")
+
+    with pytest.raises(IllegalConfiguration):
+        load_config(tmp_path / "weird.bla")
+
+
+def test_discovery_in_same_folder(tmp_path: Path) -> None:
+    (tmp_path / "setup.cfg").write_text("[pydocstyle]\nselect=1\n")
+    (tmp_path / "tox.ini").write_text("[pydocstyle]\nselect=2\n")
+    (tmp_path / "pyproject.toml").write_text('[tool.pydocstyle]\nselect="3"\n')
+
+    assert load_config(tmp_path).select == set("1")
+
+    (tmp_path / "setup.cfg").unlink()
+
+    assert load_config(tmp_path).select == set("2")
+
+    (tmp_path / "tox.ini").unlink()
+
+    assert load_config(tmp_path).select == set("3")
